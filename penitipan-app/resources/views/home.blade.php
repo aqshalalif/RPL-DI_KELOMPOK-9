33fe952a846114f7dfd3d7e743aa3281c177e329
wiicaakkk <bagus.wicaksono285@gmail.com>
2022-05-14 13:59:57 +0700

    dashboard

--- penitipan-app/resources/views/home.blade.php
+++ penitipan-app/resources/views/home.blade.php
@@ -5,18 +5,22 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+                <table class="table">
+                    <thead>
+                        <tr class="table-light">
+                          <td>ID</td>
+                          <td>Nama</td>
+                          <td>Email</td>
+                          <td>Phone</td>
+                          <td>Barang</td>
+                          <td>Berat</td>
+                        </tr>
+                    </thead>
+                  </table>
             </div>
+            <button type="submit" class="btn btn-block btn-danger">Tambah</button>
+            <button type="submit" class="btn btn-block btn-warning">Edit</button>
+            <button type="submit" class="btn btn-block btn-dark">Delete</button>
         </div>
     </div>
 </div>
