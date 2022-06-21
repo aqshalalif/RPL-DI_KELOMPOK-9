71c2067ec2c817694c5e2ef119eb9ff8104f275d
bangshadif <bangshadif@gmail.com>
2022-06-21 09:28:21 +0700

    added admin pages

---
+++ justip-app-admin/resources/views/pages/products-gallery/create.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.default')
+
+@section('content')
+    <div class="card">
+        <div class="card-header">
+            <strong>Tambah Foto Barang</strong>
+        </div>
+        <div class="card-body card-block">
+            <form action=" {{route('products-gallery.store')}} " method="POST" enctype="multipart/form-data">
+                @csrf
+                <div class="form-group">
+                    <label for="products_id" class="form-control-label">Nama Barang</label>
+                    <select name="products_id" class="form-control @error('products_id') is-invalid @enderror">
+                        @foreach ($products as $product)
+                            <option value=" {{$product->id}} "> {{$product->name}} </option>
+                        @endforeach
+                    </select>
+                    @error('products_id')
+                        <div class="text-muted"> {{$message}} </div>
+                    @enderror
+                </div>
+
+                <div class="form-group">
+                    <label for="photo" class="form-control-label">Foto Barang</label>
+                    <input type="file" name="photo" value=" {{old('photo')}} " accept="image/*" class="form-control @error('photo') is-invalid @enderror">
+                    @error('photo')
+                        <div class="text-muted"> {{$message}} </div>
+                    @enderror
+                </div>
+
+                <div class="form-group">
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="is_default" value="1" checked>
+                        <label class="form-check-label" for="is_default">
+                          Ya
+                        </label>
+                      </div>
+                      <div class="form-check">
+                        <input class="form-check-input" type="radio" name="is_default" value="0">
+                        <label class="form-check-label" for="is_default">
+                          Tidak
+                        </label>
+                      </div>
+
+                <div class="form-group">
+                    <button class="btn btn-primary btn-block" type="submit">Tambah Foto Barang</button>
+                </div>
+            </form>
+        </div>
+    </div>
+    
+@endsection
